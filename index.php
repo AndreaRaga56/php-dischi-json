29fe3fafcd102ed963b99f6dd30f2db319496297
AndreaRaga56 <andrearaga56@gmail.com>
2025-04-02 03:39:19 +0200

    feat form, style, server, index

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 $dischi = file_get_contents("./dischi.json");
-$dischi = json_decode($dischi);
+$dischi = json_decode($dischi, true);
 
 ?>
 
 
 <!DOCTYPE html>
 <html lang="it">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,21 +15,54 @@ $dischi = json_decode($dischi);
     <link rel="stylesheet" href="style.css">
     <title>Disky</title>
 </head>
+
 <body>
-    <header>
+    <header class="mb-5">
         <h1>Disky</h1>
     </header>
 
-    <main class="container mt-5">
-        <div class="row">
-            <div class="my-card col-3">
-                <div><img src="https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png" alt=""></div>
-                <h5 class="mt-3">The Dark Side of the Moon</h5>
-                <p class="mt-2">Pink Floyd</p>
-                <p class="mt-2">1973</p>
-            </div>
+    <main class="container text-center mb-5">
+        <div class="row row-cols-md-3 g-5">
+            <?php
+            foreach ($dischi as $disco) {
+                echo '<div class="col">
+                        <div class="my-card h-100">
+                            <div class="img-container"><img src="' . $disco['url_cover'] . '" alt=""></div>
+                            <h5 class="mt-3">' . $disco['titolo'] . '</h5>
+                            <p class="mt-2">' . $disco['artista'] . '</p>
+                            <p class="mt-2">' . $disco['anno_pubblicazione'] . '</p>
+                        </div>
+                    </div>';
+            } ?>
         </div>
+
+        <form class="mt-5" action="./server.php" method="POST">
+            <h2 class="m-2">Aggiungi un Nuovo Album</h2>
+            <div class="row mb-3">
+                <div class="col">
+                    <label for="titolo" class="form-label">Titolo</label>
+                    <input type="text" class="form-control" id="titolo" name="titolo" required>
+                </div>
+                <div class="col">
+                    <label for="artista" class="form-label">Artista</label>
+                    <input type="text" class="form-control" id="artista" name="artista" required>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col">
+                    <label for="anno_pubblicazione" class="form-label">Anno di Pubblicazione</label>
+                    <input type="number" class="form-control" id="anno_pubblicazione" name="anno_pubblicazione" required>
+                </div>
+                <div class="col">
+                    <label for="url_cover" class="form-label">URL Copertina</label>
+                    <input type="url" class="form-control" id="url_cover" name="url_cover" required>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Aggiungi Album</button>
+        </form>
+
 
     </main>
 </body>
+
 </html>
--- server.php
+++ server.php
@@ -1,4 +1,13 @@
 <?php
-// $dischi = file_get_contents("./dischi.json");
-// $dischi = json_decode($dischi);
-// var_dump($dischi);
+$dischi = file_get_contents("./dischi.json");
+$dischi = json_decode($dischi, true);
+
+$newDisco = $_POST;
+
+$dischi[]=$newDisco;
+
+$dischiJson = json_encode($dischi);
+
+file_put_contents("./dischi.json", $dischiJson);
+
+header('Location: ./index.php');
